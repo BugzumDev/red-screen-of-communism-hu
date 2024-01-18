dd594db1efcefa5af9eed3e1f00c38e3d09c9510
BugzumDev <80717178+BugzumDev@users.noreply.github.com>
2024-01-18 21:04:33 +0100

    Modified the HTML a little bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ var i = 1;
 function myLoop() {
     randomNum = randomNumber(100, 4500)
     setTimeout(function () {
-        document.getElementsByTagName('h1')[2].innerHTML = "Deleting files. " + i + "% complete";
+        document.getElementsByTagName('h1')[2].innerHTML = "Feljelentés... " + i + "% kész";
         i++;
         if (i < 101) {
             myLoop();
